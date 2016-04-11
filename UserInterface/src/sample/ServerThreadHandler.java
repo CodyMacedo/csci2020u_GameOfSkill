0e0915d2a37dddf488b5cef22bab8734b89d1805
alex2639 <mr.e_000@hotmail.com>
2016-04-11 16:19:39 -0400

    Cody uploaded server portion of game

--- UserInterface/src/sample/ServerThreadHandler.java
+++ UserInterface/src/sample/ServerThreadHandler.java
@@ -15,7 +15,7 @@ public class ServerThreadHandler extends Thread {
 		this.socket = socket;
 		try {
 			out = new PrintWriter(socket.getOutputStream(), true);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));//
 		} catch (IOException e) {
 			System.err.println("IOEXception while opening a read/write connection");
 		}
